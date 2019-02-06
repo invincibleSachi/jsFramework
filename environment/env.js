aabf1eae55a173460e4eae4c7dc77d30050b0da4
Sachchida Nand Singh <42642896+invincibleSachi@users.noreply.github.com>
2019-02-06 20:22:50 +0530

    Update env.js

--- environment/env.js
+++ environment/env.js
@@ -1,3 +1,12 @@
 module.exports = {
+    prod:{      
     url:'https:www.google.com',
+    dbconn:<DB_CONN_DETAILS>
+    },
+    stage:{
+        url:'https:www.google.com',
+        dbconn:<DB_CONN_DETAILS>
+    },
+    }
+
 };
